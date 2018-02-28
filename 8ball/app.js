4e0e70af3329a13589d3ea411e72391afa211137
JoseTorralba <31675524+JoseTorralba@users.noreply.github.com>
2018-02-28 18:06:23 -0500

    adds 8ball class demo

---
+++ 8ball/app.js
@@ -0,0 +1,26 @@
+const responses = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+
+    "Ask again later",
+    "Cannot predict now",
+
+    "Don't count on it",
+    "My reply is no"
+];
+
+const eightBall = document.getElementById("eight-ball");
+const answer = document.getElementById("answer");
+const question = document.getElementById("question");
+eightBall.onclick = function () {
+
+    if (question.value) {
+    const response = responses[MAth.floor(Math.random() * response.length)];
+    answer.textContent = response;
+    } else {
+        answer.textContent = "You have to ask a question first"
+    }
+}
